e7d13dfd7a8a34cb27c8e635b61dea1a8a0a7f92
Gabriel Bonaventura <gbonaventura@x-28.com>
2020-08-21 07:12:56 -0300

    first commit

---
+++ TestClassicMvvmCross.Android/Views/SplashView.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestClassicMvvmCross.Android.Views
+{
+    [Activity(Label = "SplashView")]
+    public class SplashView : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
