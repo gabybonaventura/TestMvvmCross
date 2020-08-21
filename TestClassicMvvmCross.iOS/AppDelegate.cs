8ccf58e92a0a9ba406b8022c8b917e1b1d4f122b
Gabriel Bonaventura <gbonaventura@x-28.com>
2020-08-21 08:28:12 -0300

    Add iOS

---
+++ TestClassicMvvmCross.iOS/AppDelegate.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using TestClassicMvvmCross.Core;
+using UIKit;
+
+namespace TestClassicMvvmCross.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+    }
+}
+
