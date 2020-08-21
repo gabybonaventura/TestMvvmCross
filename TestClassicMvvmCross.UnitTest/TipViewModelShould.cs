28163e6ab86e2a798fca97c27049591e5242b2e9
Gabriel Bonaventura <gbonaventura@x-28.com>
2020-08-21 09:01:59 -0300

    Basic Test

---
+++ TestClassicMvvmCross.UnitTest/TipViewModelShould.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using MvvmCross.Tests;
+using NUnit.Framework;
+using TestClassicMvvmCross.Core.Services;
+using TestClassicMvvmCross.Core.ViewModels;
+
+namespace TestClassicMvvmCross.UnitTest
+{
+    public class TipViewModelShould : MvxIoCSupportingTest
+    {
+        [Test]
+        public async Task TestThatWhenSubTotalChangeTipUpdate()
+        {
+            //Arrange
+            var calculatorService = new CalculatorService();
+            //sut: System Under Test
+            var sut = new TipViewModel(calculatorService);
+            await sut.Initialize();
+            sut.SubTotal = 200;
+            Assert.That(sut.Tip, Is.EqualTo(20));
+        }
+    }
+}
